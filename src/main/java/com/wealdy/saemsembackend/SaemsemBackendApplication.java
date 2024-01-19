95452e70b83114a6a897e8a36815dfd90ddc2412
원동연 <junhfireace@gmail.com>
2024-01-19 17:14:41 +0900

    Add: Spring Boot

---
+++ src/main/java/com/wealdy/saemsembackend/SaemsemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wealdy.saemsembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaemsemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaemsemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wealdy/saemsembackend/SaemsemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wealdy.saemsembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaemsemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
