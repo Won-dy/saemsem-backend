1059acb5d782cb57973024849f0691ea5f06390f
원동연 <junhfireace@gmail.com>
2024-03-14 18:52:54 +0900

    feat: Add List Response DTO

---
+++ src/main/java/com/wealdy/saemsembackend/common/response/ListResponseDto.java
@@ -0,0 +1,21 @@
+package com.wealdy.saemsembackend.common.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ListResponseDto<T> {
+
+    private long total;
+    private List<T> list;
+
+    public static <T> ListResponseDto<T> of(long total, List<T> list) {
+        return new ListResponseDto<>(total, list);
+    }
+
+    public static <T> ListResponseDto<T> from(List<T> list) {
+        return new ListResponseDto<>(list.size(), list);
+    }
+}
