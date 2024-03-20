a69d5964da6b11d1f61cea5cab54fc760bbfa451
원동연 <junhfireace@gmail.com>
2024-03-20 12:56:44 +0900

    feat: Add Log Utils

---
+++ src/main/java/com/wealdy/saemsembackend/common/util/LogUtils.java
@@ -0,0 +1,10 @@
+package com.wealdy.saemsembackend.common.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class LogUtils {
+
+    public static void errorLog(HttpServletRequest request, Exception exception) {
+        System.out.printf("[%s] %s - Exception Name = %s%n", request.getMethod(), request.getRequestURL(), exception.getClass().getName());
+    }
+}
