5314504b06c49beb395ef81eb55525072698c45b
원동연 <junhfireace@gmail.com>
2024-03-28 17:52:52 +0900

    Review #2 - Remove Callable
    오버 스펙

--- src/main/java/com/wealdy/saemsembackend/domain/user/controller/UserController.java
+++ src/main/java/com/wealdy/saemsembackend/domain/user/controller/UserController.java
@@ -5,7 +5,6 @@ import com.wealdy.saemsembackend.domain.core.response.Response;
 import com.wealdy.saemsembackend.domain.user.dto.UserDto;
 import com.wealdy.saemsembackend.domain.user.service.UserService;
 import jakarta.validation.Valid;
-import java.util.concurrent.Callable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,15 +20,15 @@ public class UserController {
      * 회원가입 API
      */
     @PostMapping("/user")
-    public Callable<Response<IdResponseDto>> join(@Valid @RequestBody UserDto.Create request) {
-        return () -> Response.of(userService.join(request));
+    public Response<IdResponseDto> join(@Valid @RequestBody UserDto.Create request) {
+        return Response.of(userService.join(request));
     }
 
     /**
      * 로그인 API
      */
     @PostMapping("/login")
-    public Callable<Response<UserDto.LoginResponse>> login(@Valid @RequestBody UserDto.LoginRequest request) {
-        return () -> Response.of(userService.login(request));
+    public Response<UserDto.LoginResponse> login(@Valid @RequestBody UserDto.LoginRequest request) {
+        return Response.of(userService.login(request));
     }
 }
