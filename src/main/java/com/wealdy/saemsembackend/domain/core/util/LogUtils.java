4655c5726bdf3b7801e26d54dd34f5bf8106ef2f
원동연 <junhfireace@gmail.com>
2024-03-29 12:54:57 +0900

    fix: edit Log Utils

--- src/main/java/com/wealdy/saemsembackend/domain/core/util/LogUtils.java
+++ src/main/java/com/wealdy/saemsembackend/domain/core/util/LogUtils.java
@@ -4,7 +4,11 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class LogUtils {
 
+    public static void accessLog(HttpServletRequest request, String title) {
+        System.out.printf("ACCESS --- [%s] %s (%s)%n", title, request.getRequestURI(), request.getMethod());
+    }
+
     public static void errorLog(HttpServletRequest request, Exception exception) {
-        System.out.printf("[%s] %s - Exception Name = %s%n", request.getMethod(), request.getRequestURL(), exception.getClass().getName());
+        System.out.printf("ERROR --- [%s] %s - Exception Name = %s%n", request.getMethod(), request.getRequestURL(), exception.getClass().getName());
     }
 }
