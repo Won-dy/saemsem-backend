bd29d3711282362308234fce7c14f743dcba4245
원동연 <junhfireace@gmail.com>
2024-04-17 18:43:55 +0900

    Review #21 - @Min to @PositiveOrZero

--- src/main/java/com/wealdy/saemsembackend/domain/budget/service/dto/GetBudgetDto.java
+++ src/main/java/com/wealdy/saemsembackend/domain/budget/service/dto/GetBudgetDto.java
@@ -1,7 +1,7 @@
 package com.wealdy.saemsembackend.domain.budget.service.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 
 @Getter
@@ -10,6 +10,6 @@ public class GetBudgetDto {
     @NotBlank(message = "카테고리는 필수 값입니다.")
     private String categoryName;
 
-    @Min(value = 0, message = "금액은 0원 이상으로 입력해야 합니다.")
+    @PositiveOrZero(message = "금액은 0원 이상으로 입력해야 합니다.")
     private int amount;
 }
--- src/main/java/com/wealdy/saemsembackend/domain/spending/controller/request/CreateSpendingRequest.java
+++ src/main/java/com/wealdy/saemsembackend/domain/spending/controller/request/CreateSpendingRequest.java
@@ -1,8 +1,8 @@
 package com.wealdy.saemsembackend.domain.spending.controller.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -16,7 +16,7 @@ public class CreateSpendingRequest {
     @NotNull(message = "날짜는 필수 값입니다.")
     private LocalDateTime date;
 
-    @Min(value = 0, message = "금액은 0원 이상으로 입력해야 합니다.")
+    @PositiveOrZero(message = "금액은 0원 이상으로 입력해야 합니다.")
     private long amount;
 
     @Size(max = 200, message = "메모는 200자까지 입력할 수 있습니다.")
