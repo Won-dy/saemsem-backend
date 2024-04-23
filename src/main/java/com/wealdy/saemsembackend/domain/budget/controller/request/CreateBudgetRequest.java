da18e896c89cd5f1634bfa155feca8faae2b2523
원동연 <junhfireace@gmail.com>
2024-04-23 15:45:13 +0900

    fix: DTO 의 List 에 유효성 검사 추가

--- src/main/java/com/wealdy/saemsembackend/domain/budget/controller/request/CreateBudgetRequest.java
+++ src/main/java/com/wealdy/saemsembackend/domain/budget/controller/request/CreateBudgetRequest.java
@@ -3,6 +3,7 @@ package com.wealdy.saemsembackend.domain.budget.controller.request;
 import com.wealdy.saemsembackend.domain.budget.service.dto.GetBudgetDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import java.util.List;
 import lombok.Getter;
@@ -13,5 +14,7 @@ public class CreateBudgetRequest {
     @NotBlank(message = "날짜는 필수 값입니다.")
     @Pattern(regexp = "^\\d{4}-\\d{2}$", message = "날짜 형식은 'yyyy-MM' 으로 입력해야 합니다.")
     private String date;
+
+    @NotEmpty(message = "예산은 필수 값입니다.")
     private List<@Valid GetBudgetDto> budgets;
 }
