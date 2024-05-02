33d11cc8c84a24a86c2e41fd826d2059572dce19
원동연 <junhfireace@gmail.com>
2024-05-02 10:11:12 +0900

    fix: Logger 적용 - Slf4j 및 Logback

--- src/main/java/com/wealdy/saemsembackend/domain/core/util/LogUtils.java
+++ src/main/java/com/wealdy/saemsembackend/domain/core/util/LogUtils.java
@@ -1,14 +1,16 @@
 package com.wealdy.saemsembackend.domain.core.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class LogUtils {
 
     public static void accessLog(HttpServletRequest request, String title) {
-        System.out.printf("ACCESS --- [%s] %s (%s)%n", title, request.getRequestURI(), request.getMethod());
+        log.debug("ACCESS --- [{}] {} ({})", title, request.getRequestURI(), request.getMethod());
     }
 
     public static void errorLog(HttpServletRequest request, Exception exception) {
-        System.out.printf("ERROR --- [%s] %s - Exception Name = %s%n", request.getMethod(), request.getRequestURL(), exception.getClass().getName());
+        log.error("ERROR --- [{}] {} - Exception Name = {}", request.getMethod(), request.getRequestURL(), exception.getClass().getName());
     }
 }
